6eccd56fa0a211ceab9a9e84129bcdd0c0bc513b
Thomas Martin <thomas@goldfishsound.com>
2025-04-20 17:07:00 +0200

    1st commit

--- src/Filesystem.hpp
+++ src/Filesystem.hpp
@@ -1,9 +1,11 @@
 #ifndef SRC_FILESYSTEM_HPP_
 #define SRC_FILESYSTEM_HPP_
 
-#include <experimental/filesystem>
+//#include <experimental/filesystem>
+#include <filesystem>
 
-namespace fs = std::experimental::filesystem;
+//namespace fs = std::experimental::filesystem;
+namespace fs = std::filesystem;
 
 #endif  // SRC_FILESYSTEM_HPP_
 
