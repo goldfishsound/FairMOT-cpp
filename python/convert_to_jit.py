6202a379d3ec30646097909af959e4ab5dec3b12
Thomas Martin <thomas@goldfishsound.com>
2025-04-20 17:04:57 +0200

    Added debug print statements

--- python/convert_to_jit.py
+++ python/convert_to_jit.py
@@ -7,9 +7,12 @@ import torch
 
 sys.path.append(str(Path(__file__).resolve().parent / "FairMOT" / "src" / "lib"))
 sys.path.append(str(Path(__file__).resolve().parent / "pytorch-dcnv2"))
+# print(sys.path)
 
 sys.modules["dcn_v2"] = importlib.import_module("dcn")
 sys.modules["dcn_v2"].__dict__["DCN"] = sys.modules["dcn_v2"].__dict__["DCNv2"]
+# print(sys.modules["dcn_v2"])
+# print(sys.modules["dcn_v2"].__dict__["DCN"])
 
 from models.networks.pose_dla_dcn import DLASeg
 
@@ -25,12 +28,14 @@ class DLASegCustom(DLASeg):
     def forward(self, x):
         return DLASegOutput(**super().forward(x)[-1])
 
-
 def main():
     weights_dir = Path(__file__).resolve().parents[1] / "weights"
     weights_path = weights_dir / "fairmot_dla34.pth"
     converted_weights_path = weights_dir / "fairmot_dla34_jit.pth"
+    scripted_model_path = weights_dir / "scripted_fairmot_dla34_jit.pth"
+    
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # device = torch.device("mps")
 
     model = DLASegCustom(
         "dla34",
@@ -47,9 +52,26 @@ def main():
     model = model.to(device)
     model.eval()
 
+    # scripted_model = torch.jit.script(model)
+    # print("Saving scripted model to: ", scripted_model_path)
+    # scripted_model.save(scripted_model_path)
+    
     example_input = torch.rand(1, 3, 480, 864).to(device)
-    script_module = torch.jit.trace(model, example_input)
-    script_module.save(converted_weights_path)
+    print("Tracing model")
+    traced_module = torch.jit.trace(model, example_input)
+    
+    # print("Traced module")
+    # print(traced_module)
+    
+    print("Saving model")
+    traced_module.save(converted_weights_path)
+    # print("Running model.forward")
+    # output = traced_module.forward(example_input)
+    # print("Traced output", output)
+    # print("Model graph")
+    # print(traced_module.graph)
+    # print("Model code")
+    # print(traced_module.code)
 
 
 if __name__ == "__main__":
