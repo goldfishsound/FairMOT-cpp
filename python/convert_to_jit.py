cd9a22697763f6da389840db496d7bbef5613bf1
Thomas Martin <thomas@goldfishsound.com>
2025-05-01 15:50:03 +0200

    Only exporting model on MPS device

--- python/convert_to_jit.py
+++ python/convert_to_jit.py
@@ -2,8 +2,8 @@ import importlib
 import sys
 from pathlib import Path
 from typing import NamedTuple
+
 import torch
-from models.networks.pose_dla_dcn import DLASeg
 
 sys.path.append(str(Path(__file__).resolve().parent / "FairMOT" / "src" / "lib"))
 sys.path.append(str(Path(__file__).resolve().parent / "pytorch-dcnv2"))
@@ -13,6 +13,10 @@ sys.modules["dcn_v2"] = importlib.import_module("dcn")
 sys.modules["dcn_v2"].__dict__["DCN"] = sys.modules["dcn_v2"].__dict__["DCNv2"]
 # print(sys.modules["dcn_v2"])
 # print(sys.modules["dcn_v2"].__dict__["DCN"])
+
+from models.networks.pose_dla_dcn import DLASeg
+
+
 class DLASegOutput(NamedTuple):
     hm: torch.Tensor
     wh: torch.Tensor
@@ -24,13 +28,11 @@ class DLASegCustom(DLASeg):
     def forward(self, x):
         return DLASegOutput(**super().forward(x)[-1])
 
-
 def main():
-    modelname = "fairmot_dla34"
     weights_dir = Path(__file__).resolve().parents[1] / "weights"
     weights_path = weights_dir / "fairmot_dla34.pth"
     converted_weights_path = weights_dir / "fairmot_dla34_jit.pth"
-    converted_weights_cpu_path = weights_dir / "fairmot_dla34_cpu_jit.pth"
+    scripted_model_path = weights_dir / "scripted_fairmot_dla34_jit.pth"
     
     if torch.backends.mps.is_available():
         device = torch.device("mps")        
@@ -41,6 +43,7 @@ def main():
     else:
         device = torch.device("cpu")
         print("Using CPU")
+    
 
     model = DLASegCustom(
         "dla34",
@@ -57,19 +60,26 @@ def main():
     model = model.to(device)
     model.eval()
 
-    print("Tracing model on device: ", device)    
+    # scripted_model = torch.jit.script(model)
+    # print("Saving scripted model to: ", scripted_model_path)
+    # scripted_model.save(scripted_model_path)
+    
     example_input = torch.rand(1, 3, 480, 864).to(device)
-    traced_model = torch.jit.trace(model, example_input)
-
-    print("Tracing model on device: cpu")
-    model.eval()
-    model.to("cpu")
-    traced_model_cpu = torch.jit.trace(model, example_input.to("cpu"))
-
-    print("Saving model to: ", converted_weights_path)    
-    traced_model.save(converted_weights_path)
-    print("Saving model to: ", converted_weights_cpu_path)
-    traced_model_cpu.save(converted_weights_cpu_path)
+    print("Tracing model")
+    traced_module = torch.jit.trace(model, example_input)
+    
+    # print("Traced module")
+    # print(traced_module)
+    
+    print("Saving model")
+    traced_module.save(converted_weights_path)
+    # print("Running model.forward")
+    # output = traced_module.forward(example_input)
+    # print("Traced output", output)
+    # print("Model graph")
+    # print(traced_module.graph)
+    # print("Model code")
+    # print(traced_module.code)
 
 
 if __name__ == "__main__":
