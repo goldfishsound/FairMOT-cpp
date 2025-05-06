291bc006f41aebfed199cde8ef27c3adfe67e1c9
Thomas Martin <thomas@goldfishsound.com>
2025-05-06 13:00:05 +0200

    WIP

--- test/my_test.cpp
+++ test/my_test.cpp
@@ -1,15 +1,31 @@
 // my_test.cpp
 #include <gtest/gtest.h>
 #include "../src/api/include/FairMotAPI.hpp"
+#include <fstream>
+#include <iostream>
+#include <c10/util/Exception.h>
+
+std::string model_path = "/Users/thomas/Developer/projects/FairMOT-cpp/weights/fairmot_dla34_jit.pth";
+
+
 
 // A simple function to test
 int Add(int a, int b) {
     return a + b;
 }
 
+// AWTracker::Tracker tracker();
+// AWTracker::Tracker tracker(model_path, 25.0, 50, 120);
+// std::cout << "Tracker constructed successfully" << std::endl;
+
 TEST(LibraryTest, LoadLibrary) {
-    tracker::Tracker tracker;
-    tracker.SetScoreThreshold(0.5);
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != nullptr) {
+        std::cout << "Current working directory: " << cwd << std::endl;
+    } else {
+        perror("getcwd() error");
+    }
+    AWTracker::Tracker tracker;
 
 }
 
