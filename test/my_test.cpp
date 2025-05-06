28a1c576937ffc2c16a3a0e6610722f21ede6842
Thomas Martin <thomas@goldfishsound.com>
2025-05-06 16:11:47 +0200

    Removed old add tests Added test for setter en getter of scoreThreshold Added test for TrackImage

--- test/my_test.cpp
+++ test/my_test.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <iostream>
 #include <c10/util/Exception.h>
+#include <opencv2/opencv.hpp>
 
 std::string model_path = "/Users/thomas/Developer/projects/FairMOT-cpp/weights/fairmot_dla34_jit.pth";
 
@@ -14,29 +15,50 @@ int Add(int a, int b) {
     return a + b;
 }
 
-// AWTracker::Tracker tracker();
-// AWTracker::Tracker tracker(model_path, 25.0, 50, 120);
-// std::cout << "Tracker constructed successfully" << std::endl;
-
-TEST(LibraryTest, LoadLibrary) {
-    char cwd[1024];
-    if (getcwd(cwd, sizeof(cwd)) != nullptr) {
-        std::cout << "Current working directory: " << cwd << std::endl;
-    } else {
-        perror("getcwd() error");
-    }
+TEST(LibraryTest, DefaultConstructor) {
     AWTracker::Tracker tracker;
-
+    // ASSERT_EQ(tracker, !nullptr);
 }
 
-// Test case: Add
-TEST(AdditionTest, HandlesPositiveNumbers) {
-    EXPECT_EQ(Add(2, 3), 5);
+TEST(LibraryTest, ParameterizedConstructor) {
+    AWTracker::Tracker tracker(model_path, 25.0, 50, 120);
+    // ASSERT_EQ(tracker, !nullptr);
 }
 
-TEST(AdditionTest, HandlesNegativeNumbers) {
-    EXPECT_EQ(Add(-2, -3), -5);
+TEST(LibraryTest, SetScoreThreshold) {
+    AWTracker::Tracker tracker(model_path, 25.0, 50, 120);
+    tracker.SetScoreThreshold(0.5);
+    EXPECT_EQ(tracker.GetScoreThreshold(), 0.5);
 }
+
+TEST(LibraryTest, TrackImage)
+{
+    std::string project_root = "/Users/thomas/Developer/projects/FairMOT-cpp"; // Replace with your project root path
+    std::string image_path = project_root + "/test/assets/threePeople.jpg";
+    cv::Mat image = cv::imread(image_path);
+    ASSERT_FALSE(image.empty()) << "Failed to load image";
+
+    int width = image.cols;
+    int height = image.rows;
+    std::cout << "Image dimensions: " << width << "x" << height << std::endl;
+    // if (image.type() != CV_8UC3) {
+    //     image.convertTo(image, CV_8UC3);
+    // }
+    const unsigned char* image_data = image.data;
+
+    AWTracker::Tracker tracker(model_path, 25.0, 50, 120);
+    std::vector<AWTracker::Tracker::TrackOutput>  results = tracker.TrackImage(image_data, height, width);
+    
+    std::cout << "Number of tracking results: " << results.size() << std::endl;
+    EXPECT_FALSE(results.empty()) << "Tracking results should not be empty";
+    EXPECT_EQ(results.size(), 3) << "Expected 3 tracking results";
+    for (const auto& result : results) {
+        std::cout << "Track ID: " << result.track_id << ", Score: " << result.score
+                  << ", Bounding Box: [" << result.tlwh[0] << ", " << result.tlwh[1]
+                  << ", " << result.tlwh[2] << ", " << result.tlwh[3] << "]" << std::endl;
+    }
+}
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
