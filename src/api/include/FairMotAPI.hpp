ff03b344955b9e9b6c46d966679fed2c84301d46
Thomas Martin <thomas@goldfishsound.com>
2025-05-13 10:36:35 +0200

    Removed namespace AWTracker

--- src/api/include/FairMotAPI.hpp
+++ src/api/include/FairMotAPI.hpp
@@ -27,12 +27,12 @@ public:
         float score;
     };
 
-    // Default constructor
-    Tracker();
+  // Default constructor
+  Tracker();
 
-    // Parameterized constructor
-    Tracker(const std::string &rModelPath, double frameRate,
-            int maxPerImage, int trackBuffer);
+  // Parameterized constructor
+  Tracker(const std::string &rModelPath, double frameRate,
+          int maxPerImage, int trackBuffer);
 
     // Destructor
     ~Tracker();
@@ -40,13 +40,13 @@ public:
     // Copy constructor
     Tracker(const Tracker &other);
 
-    // Method to track objects in the image
-    // This function takes an image and a tracker object as input and returns a vector of TrackOutput
-    // containing the tracking results.
-    std::vector<Tracker::TrackOutput> TrackImage(const unsigned char* rImage, const int height, const int width);
+  // Method to track objects in the image
+  // This function takes an image and a tracker object as input and returns a vector of TrackOutput
+  // containing the tracking results.
+  std::vector<Tracker::TrackOutput> TrackImage(const unsigned char *rImage, const int height, const int width);
 
-    // Method to get the score threshold
-    double GetScoreThreshold() const;
+  // Method to get the score threshold
+  double GetScoreThreshold() const;
 
     // Method to set the score threshold
     void SetScoreThreshold(double threshold);
